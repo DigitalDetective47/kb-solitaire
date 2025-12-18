229ac311f2f778fe704e61a603f0d4e8f3aac616
DigitalDetective47 <ninji2701@gmail.com>
2025-12-17 21:28:04 -0500

    add card type

---
+++ card.h
@@ -0,0 +1,26 @@
+typedef unsigned char Card;
+
+#define ACE 1
+#define JACK 11
+#define QUEEN 12
+#define KING 13
+
+enum Suit
+{
+    SPADES = 0,
+    CLUBS = 1,
+    HEARTS = 2,
+    DIAMONDS = 3,
+};
+
+enum Color
+{
+    BLACK = 0,
+    RED = 1,
+};
+
+#define RANK(c) ((c) & 15)
+#define SUIT(c) ((c) >> 4)
+#define COLOR(c) ((c) >> 5)
+
+#define CARD(rank, suit) ((rank) + ((suit) << 4))
