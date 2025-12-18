8b8861aa267ddfc0a20b64ad23fe5df356569c2a
DigitalDetective47 <ninji2701@gmail.com>
2025-12-17 22:28:10 -0500

    seed random number generator

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "card.h"
+#include <stdlib.h>
+#include <time.h>
 
 struct CardPile {
   Card cards[24];
@@ -10,4 +12,7 @@ struct CardPile drawPile;
 struct CardPile tableau[7];
 Card foundation[4];
 
-int main() { return 1; }
+int main() {
+  srand(time(NULL));
+  return 1;
+}
